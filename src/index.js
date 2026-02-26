49b902b7b8485a1a4f89ea2839d58bd6ff02b6db
ThulaneEjoobi <thulanet@ejoobi.co.za>
2026-02-26 21:46:55 +0200

    Fix GitHub Pages black page using HashRouter

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-
+import { HashRouter } from 'react-router-dom'; // <-- import HashRouter
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <HashRouter>
+    <App />
+  </HashRouter>
+);
